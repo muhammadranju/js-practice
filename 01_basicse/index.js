89a4d01dbf9e6e8cfb155f1be5a672971e928f79
Muhāmmad Ranju <80270685+muhammadranju@users.noreply.github.com>
2023-10-01 17:44:56 +0000

    ok

--- 01_basicse/index.js
+++ 01_basicse/index.js
@@ -1,5 +1,59 @@
-console.log("Hello wordl");
-const fristName="Md"
-const lastName="Ranju"
+// console.log("Hello wordl");
+// const fristName="Md"
+// const lastName="Ranju"
 
-console.log(fristName.concat(' ',lastName));
+// console.log(fristName.concat(' ',lastName));
+
+
+
+// const myName=['MD',"Ranju","Amin"]
+// const myName2=['Muhammad', "Ranju",'Ranju']
+
+// const allName=myName.concat(myName2)
+// console.log(allName);
+
+// const allName2=[...myName, ...myName2]
+// console.log(allName2);
+
+
+// console.log(Array.isArray('Md Ranju'));
+// console.log(Array.from({ }));
+
+// let score1=100
+// let score2=200
+// let score3=300
+
+// console.log(Array.of(score1,score2,score3));
+
+
+
+ const obj={
+    name:"Md Ranju",
+    age:20,
+    work:"Development",
+    time:"10am-8pm"
+ }
+ const obj2={
+    name:"Md Amin",
+    age:20,
+    work:"Programming",
+    time:"10am-8pm"
+ }
+ const obj3={
+    name:"Muhammad Ranju",
+    age:20,
+    work:"JavaScript Programmer",
+    time:"10am-8pm"
+ }
+
+
+
+//  const allOjb=[
+//     obj,obj2,obj3
+//  ]
+
+ const spreddObj={...obj, ...obj2,}
+
+//  console.log(spreddObj);
+//  console.log(Object.keys(obj3));
+//  console.log(Object.entries(obj3));
