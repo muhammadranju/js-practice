97e2a2fd99c39f3b8b7af70819f6c45f6722baa8
Muhāmmad Ranju <80270685+muhammadranju@users.noreply.github.com>
2023-10-03 18:08:35 +0000

    arrow

---
+++ 02_basics/03_arrow.js
@@ -0,0 +1,10 @@
+const arr = { gfj: "df" };
+// const arr = [25];
+console.log(arr);
+console.log(Object.keys(arr));
+
+if (Object.keys(arr).length !== 0) {
+  console.log("True");
+} else {
+  console.log("False");
+}
