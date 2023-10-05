f009a078126a7b53a492f060299a1d063027a4f6
Muhāmmad Ranju <80270685+muhammadranju@users.noreply.github.com>
2023-10-05 04:02:20 +0000

    arrow

--- 02_basics/03_arrow.js
+++ 02_basics/03_arrow.js
@@ -1,10 +1,40 @@
 const arr = { gfj: "df" };
 // const arr = [25];
-console.log(arr);
-console.log(Object.keys(arr));
+// console.log(arr);
+// console.log(Object.keys(arr));
 
-if (Object.keys(arr).length !== 0) {
-  console.log("True");
-} else {
-  console.log("False");
-}
+// if (Object.keys(arr).length !== 0) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+const coding = ["js", "roby", "java", "python", "cpp"];
+// const codes = coding.forEach((value) => {
+//   // return value;
+//   console.log(value);
+// });
+
+const codes = coding.map((value) => {
+  return value;
+  //   console.log(value);
+});
+// console.log(codes);
+
+const myCoding = [
+  {
+    langName: "JavaScript",
+    langfileName: "js",
+  },
+  {
+    langName: "Java",
+    langfileName: "java",
+  },
+  {
+    langName: "Pyrhon",
+    langfileName: "py",
+  },
+];
+
+myCoding.forEach((items) => {
+  console.log(items.langName, ":", items.langfileName);
+});
