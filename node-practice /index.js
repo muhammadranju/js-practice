289d60b78b8fee3fb56dfe4bf384ea5d069c92aa
Muhāmmad Ranju <80270685+muhammadranju@users.noreply.github.com>
2023-10-07 23:52:16 +0600

    Delete node-practice  directory

--- node-practice /index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3030;
-app.get("/", (req, res) => {
-  return res.status(200).send("Hello");
-});
-app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
